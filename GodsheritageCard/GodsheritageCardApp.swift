e8afb58ed9b4fe9ef806ae60b5f01db23b97b191
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-02-16 01:24:10 -0500

    Initial Commit

---
+++ GodsheritageCard/GodsheritageCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GodsheritageCardApp.swift
+//  GodsheritageCard
+//
+//  Created by Godsheritage Adeoye on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GodsheritageCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
